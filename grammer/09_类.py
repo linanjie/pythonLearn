96e9a45cb3c0dbe36084030fcc48d378c1f6a7d2
linanjie5918@hotmail.com <linanjie5918@hotmail.com>
2020-01-13 17:51:29 +0800

    哈哈哈哈哈哈；

--- grammer/09_类.py
+++ grammer/09_类.py
@@ -1,1 +1,12 @@
-# 
+# 类基本语法
+class Dog():
+    # __init__() 是一个特殊的方法，每当你根 据Dog 类创建新实例时，Python都会自动运行它
+    # 在这个方法的名称中，开头和末尾各有两个下划线，这是一种约定，旨在避免Python默认方法与普通方法发生名称冲突
+    def __init__(self,name,age):
+        self.name = name
+        self.age = age
+    def sit(self):
+        print(self.name.title()+" is now sitting")
+    def roll_over(self):
+        print(self.name.title()+" rolled over")
+
